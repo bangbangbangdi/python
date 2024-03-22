c5e01cf18fc53832002514a22aac2f3205d05c89
bangbangbang <19071916@qq.com>
2024-03-22 13:55:40 +0900

    add-21点执行逻辑

--- Basic/10_3扑克牌21点.py
+++ Basic/10_3扑克牌21点.py
@@ -4,7 +4,7 @@ import random
 # 21点扑克牌游戏python实现
 # 游戏者的目标是使手中的牌的点数之和不超过21点且尽量大。
 # 计算规则是：
-# 2至9牌，按其原点数计算,A算作1点
+# 2至9牌，按其原点数计算,A算作1点或者11点(例如 A(1) + J(10) = 21 ; A(1) + 9 + J(10) = 20)
 # 10、J、Q、K牌都算作10点（一般记作T，即Ten）
 """ 
 21点扑克牌游戏设计思路
@@ -22,8 +22,9 @@ import random
 如果庄家的点数之和超过21,则玩家获胜。
 
 最后， 比较玩家和庄家的点数。如果玩家的点数大，则获胜。如果玩家的点数小，则输牌。
-如果点数相同，则平局。但玩家和庄家的牌值都是21点，此时拥有blackjack (一张Ace 和一张点数为10的牌)方获胜。
+如果点数相同，则平局。但玩家和庄家的牌值都是21点，此时拥有blackjack (一张Ace 和一张点数为10的牌)方获胜,双方都是blackJack则平局。
 """
+
 
 # -------------------- 构建模型 --------------------
 
@@ -101,8 +102,60 @@ class Player:
         """展示自己的手牌"""
         print(self.cards)
 
-# -------------------- 游戏逻辑 --------------------
-# 根据手牌判断得分
 
-# 判断输赢
-# def winner(p1:Player, p2:Player):
+# -------------------- 游戏逻辑 --------------------
+def is_blackjack(cards: list[Card]) -> bool:
+    """
+    判断是否是一张A和一张10或J,Q,K
+    :param cards: 手牌列表
+    :return:是否是blackJack
+    """
+    if len(cards) != 2:
+        return False
+    if cards[0].face != 1 and cards[1].face != 1:
+        return False
+    if cards[0].face == 1 and not (cards[1].face in range(10, 14)):
+        return False
+    if cards[1].face == 1 and not (cards[0].face in range(10, 14)):
+        return False
+    return True
+
+
+# print(is_blackjack([Card(Suite.SPADE, 1), Card(Suite.SPADE, 13)]))
+# print(is_blackjack([Card(Suite.SPADE, 1), Card(Suite.SPADE, 10)]))
+# print(is_blackjack([Card(Suite.SPADE, 2), Card(Suite.SPADE, 10)]))
+# print(is_blackjack([Card(Suite.SPADE, 1), Card(Suite.SPADE, 10), Card(Suite.SPADE, 10)]))
+
+
+def get_count(cards: list[Card]) -> int:
+    """
+    根据手牌判断得分,如果是blackjack则返回22分
+    :param cards: 手牌列表
+    :return: 手牌对应的得分
+    """
+    if is_blackjack(cards):
+        return 22
+    count = 0
+    for card in cards:
+        count += card.face
+    return count
+
+
+# cs = [Card(Suite.HEART, 10), Card(Suite.SPADE, 11)]
+# print(get_count(cs))
+
+def winner(p1: Player, p2: Player) -> int:
+    """
+    判断输赢 - 这个有个潜台词,即能来到这一步的双方手牌分数均不超过21点
+    :param p1: 玩家1
+    :param p2: 玩家2
+    :return: 获胜的玩家 0-平局; 1-玩家1获胜; 2-玩家2获胜
+    """
+    cards1 = p1.cards
+    cards2 = p2.cards
+    if get_count(cards1) == get_count(cards2):
+        return 0
+    elif get_count(cards1) > get_count(cards2):
+        return 1
+    else:
+        return 2
