de08ccd5918339cb2a6efc14b63210b22270ca73
bangbangbang <19071916@qq.com>
2024-04-25 22:47:37 +0900

    21号下午一些演示代码(没啥价值...不需要看)~ 时间过的好快喔... (新地方终于通网啦!! )

--- Test.py
+++ Test.py
@@ -1,7 +1,45 @@
-def test(name=''):
+from enum import Enum
+
+
+def test(name):
     print(__name__)
     print(f"Hello {name}!")
 
 
+# 牌
+class Suite(Enum):
+    """黑桃、红心、方块、梅花"""
+    SPADE, HEART, DIAMOND, CLUB = range(4)
+
+
+class Card:
+    def __init__(self, suite, face):
+        self.suite = suite
+        self.face = face
+
+    def __repr__(self):
+        suite = '♠♥♦♣'
+        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return f'{suite[self.suite.value]}{faces[self.face]}'
+
+
 if __name__ == '__main__':
-    test()
+    # card = Card(suite=Suite.HEART, face=4)
+    # print(card)
+    # s = '你好阿'
+    # print(s[0])
+    # cards = []
+    # for suite in Suite:
+    #     for face in range(1, 14):
+    #         cards.append(Card(suite=suite, face=face))
+    # print(cards)
+
+    # cards = [i for i in range(10)]
+    # cards = [s.value for s in Suite]
+    cards = [Card(suite, face) for suite in Suite for face in range(1, 14)]
+    print(cards)
+
+    # cards = [(suite.value, face) for suite in Suite for face in range(1, 14)]
+    # cards = [Card(suite, face) for suite in Suite for face in range(1, 14)]
+    # cards = [Card(suite, face) for suite in Suite for face in range(1, 14)]
+    # print(cards)
--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import Test
 
 print('----------------')
 Test.test('Ding')
+
+name, age, gender = range(3)
+
+
+for i in range(3):
+    print(i)
