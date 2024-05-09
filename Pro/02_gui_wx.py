e25c26edeb7a20dfb80f4ca7fb8784fd44c320e8
bangbangbang <19071916@qq.com>
2024-05-10 00:53:32 +0900

    找到了有关wxPython更详细的教程(原有的教程过于简略了...) 追加了一些注释.

--- Pro/02_gui_wx.py
+++ Pro/02_gui_wx.py
@@ -5,17 +5,37 @@ import wx
 '''
 wxPython是python语言的一套优秀的GUI(Graphical user interface 图形用户界面)库
 允许python程序员很方便的创建完整的、功能健全的GUI用户界面.
+
+reference from https://github.com/necan/wxPython-tutorial/blob/master
 '''
 
 
 # -- 第一个wx程序
 # 使用之前记得要先安装wxPython库哈~
 def first_wx():
+    # 创建一个应用程序对象(每一个wxPython程序都必须又一个应用程序对象)
     app = wx.App()
-    frm = wx.Frame(None, title='BLAME', size=(600, 600), pos=(100, 100))
+
+    # 创建了一个wx.Frame对象. wx.Frame是其他组件的父组件,以后我们会经常见到它.
+    frm = wx.Frame(None, title='First wxPython application')
+    # 创建了wx.Frame之后,还必须调用Show()方法将其显示在屏幕上.
+    frm.Show()
+
+    app.MainLoop()
+
+
+def gui_kino():
+    # 创建一个应用程序对象(每一个wxPython程序都必须又一个应用程序对象)
+    app = wx.App()
+
+    frm = wx.Frame(None, title='BLAME', style=wx.RESIZE_BORDER | wx.SYSTEM_MENU, size=(600, 600), pos=(100, 100))
+    # 创建wx.Panel组件,并指定其父容器
     panel = wx.Panel(frm, size=(200, 200), pos=(100, 100))
+    # 创建image对象,指定图片路径以及类型
     image = wx.Image('./img/kino1.png', wx.BITMAP_TYPE_PNG)
     my_pic = image.ConvertToBitmap()
+
+    frm.Center()
 
     # 显示图片
     wx.StaticBitmap(panel, -1, my_pic)
