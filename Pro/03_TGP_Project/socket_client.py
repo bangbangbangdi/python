f282ea1a11502a9e9b6dae0c0dd47f17ced436ea
bangbangbang <19071916@qq.com>
2024-05-23 00:35:21 +0900

    追加了socket通信的内容

--- Pro/03_TGP_Project/socket_client.py
+++ Pro/03_TGP_Project/socket_client.py
@@ -1,1 +1,22 @@
 # -------------------- socket-客户端 --------------------
+import socket
+
+# 创建socket对象
+sock = socket.socket()
+# 连接服务器 (了解即可:localhost多数情况下都对应着127.0.0.1; 后面的8995是端口号)
+sock.connect(('localhost', 8995))
+# sock.connect(("127.0.0.1",8995))
+
+while True:
+    send_data = input("Enter to send message: ")
+
+    # 对输入的内容 是使用指定字符集编码(这里指定的字符集是utf8)
+    encoded = send_data.encode('utf8')
+    # 发送数据到服务器(编码后)
+    sock.send(encoded)
+    # 等待服务器的响应,此处的1024是指要接收的最大数据量(单位是字节)
+    accept_data = sock.recv(1024)
+    # 对接收到的服务器响应进行解码
+    decoded_data = accept_data.decode('utf8')
+    # 打印服务器的响应
+    print('Received message: ', decoded_data)
--- Pro/03_TGP_Project/socket_server.py
+++ Pro/03_TGP_Project/socket_server.py
@@ -1,1 +1,28 @@
 # -------------------- socket-服务器 --------------------
+import socket
+
+# 创建socket对象
+sock = socket.socket()
+# 绑定ip和端口号
+sock.bind(('0.0.0.0', 8995))
+# 设置监听,这里的5指的是socket的最大排队个数,了解即可(详情参考:https://blog.51cto.com/u_3078781/3288071)
+sock.listen(5)
+# 等待客户端连接
+conn, addr = sock.accept()
+
+# 打印连接信息
+print(conn, addr)
+while True:
+    # 接收客户端的请求内容,最大数据量为1024个字节
+    accept_data = conn.recv(1024)
+    # 对数据解码
+    decoded_data = accept_data.decode('utf-8')
+    # 打印解码后的内容
+    print('received data:', decoded_data)
+
+    # 设置给客户端响应的内容
+    send_data = 'received!'
+    # 对响应内容进行编码
+    encoded_data = send_data.encode('utf-8')
+    # 给客户端返回响应内容
+    conn.send(encoded_data)
