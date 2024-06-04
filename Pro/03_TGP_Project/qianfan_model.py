f458f7ef83f9b1ebf4a78cd85c23e1278287fafb
bangbangbang <19071916@qq.com>
2024-06-05 00:35:48 +0900

    追加了联系上下文的方法
    另外之前的wxPython macOS界面卡顿的问题也查了一下,明天试试看能不能解决

--- Pro/03_TGP_Project/qianfan_model.py
+++ Pro/03_TGP_Project/qianfan_model.py
@@ -31,12 +31,37 @@ def chat(question):
         }]
     )
 
-    print(resp.body['result'])
+    print(resp.body)
+
+
+# ---------------- 上述代码没办法联系上下文,因此需要再改进一下 ----------------
+
+def chat2():
+    os.environ["QIANFAN_AK"] = "PvSiq0beNph79ljuFcCqFPsI"
+    os.environ["QIANFAN_SK"] = "ict6qgdnVdu0zs6tNJRexENlt9JCItoY"
+
+    robot = qianfan.ChatCompletion()
+    message = []
+    while True:
+        question = input("Please enter your question: ") or "exit"
+        if question == "exit":
+            break
+        que_dic = {"role": "user", "content": question}
+        message.append(que_dic)
+        resp = robot.do(
+            endpoint="ernie-char-8k",
+            messages=message
+        )
+        print(resp.body['result'])
+        message.append({"role": "assistant", "content": resp.body["result"]})
 
 
 def main():
-    question = '奇诺之旅是什么样的作品?'
-    chat(question)
+    # question = '奇诺之旅怎么样的作品?'
+    # question = '奇诺是谁?'
+    # chat(question)
+
+    chat2()
 
 
 if __name__ == '__main__':
