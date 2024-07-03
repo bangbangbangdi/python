555a94bbd21936b34d73db1444f6a026eb2f40a9
bangbangbang <19071916@qq.com>
2024-07-03 23:55:31 +0900

    今天狠狠的补了一下进度 1.追加了文本输入框,并居中 2.追加了聊天记录框 3.追加了文心一言的模块 (下次在将AI返回的文本展示到界面上,这个项目就可以告一段落了.)
    PS:周一写日记去了、周二看雾山五行去了

--- Pro/03_TGP_Project/qianfan_gui.py
+++ Pro/03_TGP_Project/qianfan_gui.py
@@ -1,5 +1,7 @@
 # -------------------- 使用千帆AI模型 + GUI实现一个AI聊天程序 --------------------
 import wx
+import os
+import qianfan
 
 
 class MyPanel(wx.Panel):
@@ -7,6 +9,18 @@ class MyPanel(wx.Panel):
         super(MyPanel, self).__init__(parent)
         self.background_image = wx.Bitmap('../img/kuroniko.jpeg')
         self.Bind(wx.EVT_PAINT, self.on_paint)
+        # 创建聊天记录框
+        self.text = wx.TextCtrl(self, size=(450, 300), style=wx.TE_READONLY)
+        # 创建输入文本框 并使其支持回车事件
+        self.input_text = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
+        # 初始化文本输入框
+        self.init_input_text()
+        # 设定输入框的背景颜色,其中128是其透明度(macOS无效)
+        self.input_text.SetBackgroundColour(wx.Colour(0, 0, 0, 128))
+        # 按下回车时触发on_enter函数
+        self.input_text.Bind(wx.EVT_TEXT_ENTER, self.on_enter)
+        # 输入框自动获取焦点
+        self.input_text.SetFocus()
 
     def on_paint(self, event):
         dc = wx.PaintDC(self)
@@ -14,7 +28,52 @@ class MyPanel(wx.Panel):
         image = self.background_image.ConvertToImage()
         image = image.Scale(size.GetWidth(), size.GetHeight(), wx.IMAGE_QUALITY_HIGH)
         resized_bitmap = wx.Bitmap(image)
-        dc.DrawBitmap(resized_bitmap, 0, 0, False)
+        dc.DrawBitmap(resized_bitmap, 0, 0, True)
+        self.text.SetSize(450, size.GetHeight() - 100)
+
+    def init_input_text(self):
+        self.input_text.SetMinSize(wx.Size(450, -1))
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox.AddStretchSpacer()
+        hbox.Add(self.text, flag=wx.ALIGN_CENTER)
+        hbox.AddStretchSpacer()
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        hbox2.AddStretchSpacer()
+        hbox2.Add(self.input_text, flag=wx.ALIGN_CENTER)
+        hbox2.AddStretchSpacer()
+
+        vbox.Add(hbox, flag=wx.EXPAND | wx.TOP, border=20)
+        vbox.AddStretchSpacer()
+        vbox.Add(hbox2, flag=wx.EXPAND | wx.BOTTOM, border=20)
+
+        self.SetSizer(vbox)
+
+    def on_enter(self, event):
+        # 获取输入框内容
+        text = self.input_text.GetValue()
+        print(text)
+        # 清空输入框
+        self.input_text.Clear()
+        self.chat(text)
+
+    def chat(self, question):
+        os.environ["QIANFAN_AK"] = "PvSiq0beNph79ljuFcCqFPsI"
+        os.environ["QIANFAN_SK"] = "ict6qgdnVdu0zs6tNJRexENlt9JCItoY"
+
+        robot = qianfan.ChatCompletion()
+        message = []
+        que_dic = {"role": "user", "content": question}
+        message.append(que_dic)
+        resp = robot.do(
+            endpoint="ernie-char-8k",
+            messages=message
+        )
+        print(resp.body['result'])
+        message.append({"role": "assistant", "content": resp.body["result"]})
 
 
 class QianfanFrame(wx.Frame):
@@ -30,10 +89,8 @@ class QianfanFrame(wx.Frame):
         # 绑定事件 当按下cmd+Q时触发 on_quit函数
         # (这里为什么会绑定cmd+Q我也没有查到,只能暂时认为是在不指定绑定的快捷键的情况下默认就绑定的是cmd+Q吧)
         self.Bind(wx.EVT_MENU, self.on_quit)
-
+        # 添加panel
         self.panel = MyPanel(self)
-
-        # 设置背景图片
 
     def on_quit(self, event):
         self.Close(True)
